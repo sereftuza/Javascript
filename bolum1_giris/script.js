d42a1d2f355fd97bc03803dd6e33e2c62dc0e0a7
srftuza <sereftuza@gmail.com>
2021-05-20 09:19:04 +0200

    script

---
+++ bolum1_giris/script.js
@@ -0,0 +1,33 @@
+// alert("dikkat");
+// console.log("Hello javascript");
+
+// console.log("Javascript javadan kolay bir dildir...");
+// console.error("bu bir hatadir");
+// console.warn("bu bir uyaridir");
+
+// ** degisken tanimlama
+
+var pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+pi = "3";
+console.log(pi);
+console.log(typeof pi);
+
+pi = true;
+console.log(pi);
+console.log(typeof pi);
+
+var isim = "Ahmet";
+console.log(isim);
+console.log(typeof isim);
+isim = 4;
+console.log(isim);
+console.log(typeof isim);
+
+const pisayisi = 3.14;
+console.log(pisayisi);
+
+pisayisi = 3;
+console.log(pisayisi);
